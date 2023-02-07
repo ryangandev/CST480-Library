11b03969d7ba4b2878ba3011628e84c608585540
Ryan Gan <49723210+ryanzg123@users.noreply.github.com>
2023-02-07 18:16:44 -0500

    update authortab;e to have delete functions

--- front/src/Components/AuthorTable.tsx
+++ front/src/Components/AuthorTable.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-
+import DeleteIcon from '@mui/icons-material/Delete';
 
 interface Author {
     id: number;
@@ -28,14 +28,43 @@ const AuthorTable: React.FC = () => {
         fetchAuthors();
     }, []);
 
-    const handleAuthorDelete = (event: React.MouseEvent<HTMLButtonElement>) => {
-        
+    const handleAuthorDelete = async (event: React.MouseEvent<HTMLButtonElement>) => {
+        const authorId = event.currentTarget.value;
+        if (window.confirm(`Are you sure you want to delete the author with id: ${authorId}?`)) {
+            try {
+                const response = await axios.delete(`/api/authors/${authorId}`);
+                console.log(response.data);
+                // Re-render the table here
+                const fetchAuthors = async () => {
+                    const response = await axios.get('/api/authors');
+                    const authors = await response.data;
+                    setAuthors(authors.authors);
+                };
+                fetchAuthors();
+            } catch (error) {
+                console.log(error);
+            }
+        }
     };
 
     const columns: GridColDef[] = [
         { field: 'id', headerName: 'Author No.', width: 150, headerAlign: "center" },
         { field: 'name', headerName: 'Author Name', width: 250, headerAlign: "center" },
         { field: 'bio', headerName: 'Author Bio', width: 300, headerAlign: "center" },
+        { field:
+            'action',
+            headerName: '',  
+            width: 150,
+            headerAlign: "center",
+            renderCell: (params) => {
+                return <>
+                    <button 
+                        value={params.id} 
+                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-2 rounded"
+                        onClick={handleAuthorDelete}><DeleteIcon /></button>
+                    </>
+            }
+        },
       ];
 
     const rows = Array.isArray(authors) ? authors.map((row) => ({
@@ -45,7 +74,7 @@ const AuthorTable: React.FC = () => {
     })) : [];
 
     return (
-        <div className="w-1/2 mx-auto my-10">
+        <div className="w-2/3 mx-auto my-10">
             <h2 className="text-2xl font-bold mb-5">Authors</h2>
             <div style={{ height: 700, width: '100%' }}>
                 <DataGrid
